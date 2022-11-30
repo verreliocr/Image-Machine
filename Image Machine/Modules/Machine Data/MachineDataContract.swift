f94775b29b0171a65dc499efafb34d217d696baa
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 05:40:44 +0700

    [Verrel] add contract for machine data module

--- Image Machine/Modules/Machine Data/MachineDataContract.swift
+++ Image Machine/Modules/Machine Data/MachineDataContract.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+protocol IMachineDataView: AnyObject {
+    func showMessage(_ message: String, title: String, completion: (() -> Void)?)
+}
+
+protocol ILoginViewModel {
+    func setView(_ view: IMachineDataView)
+}
