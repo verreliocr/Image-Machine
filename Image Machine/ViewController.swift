9ecb4f18a3d74c2bd898e7a8b8b995a8b378386c
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 05:05:34 +0700

    Initial Commit

---
+++ Image Machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
