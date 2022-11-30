7435f8e8ad5579c7cf5abb2a1c773074a96e1758
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 05:43:56 +0700

    [Verrel] add machine data model

---
+++ Image Machine/Modules/Machine Data/MachineDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  MachineDataModel.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import Foundation
+
+class MachineDataModel {
+    var data: [MachineObject] = []
+    var sortedBy = "name"
+    var sortedType = "asc"
+}
