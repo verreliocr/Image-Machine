f7a3ffc12f9c81c1047604f261d3e7f82b9479c0
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 05:35:11 +0700

    [Verrel] add machine data object

---
+++ Image Machine/Modules/Machine Data/MachineDataContract.swift
@@ -0,0 +1,8 @@
+//
+//  MachineDataContract.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import Foundation
---
+++ Image Machine/Objects/MachineObject.swift
@@ -0,0 +1,16 @@
+//
+//  MachineObject.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import Foundation
+
+struct MachineObject {
+    var id: String
+    var name: String
+    var type: String
+    var qrCode: Int
+    var lastMaintenance: Date
+}
