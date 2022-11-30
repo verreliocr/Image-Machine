f1c3aac4b2428f21f494463a9eb59612317d7f35
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 06:11:11 +0700

    [Verrel] create view for machine data module

---
+++ Image Machine/Modules/Machine Data/MachineDataView.swift
@@ -0,0 +1,20 @@
+//
+//  MachineDataView.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import UIKit
+
+class MachineDataView: UIViewController {
+
+    @IBOutlet weak var sortNameButton: UIButton!
+    @IBOutlet weak var sortTypeButton: UIButton!
+    @IBOutlet weak var machineDataTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
