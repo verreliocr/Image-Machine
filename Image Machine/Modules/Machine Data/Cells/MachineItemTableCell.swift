24e67b42bf3275ceabbf089c76a454246f743acf
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 07:53:28 +0700

    [Verrel] add action button for edit and delete

--- Image Machine/Modules/Machine Data/Cells/MachineItemTableCell.swift
+++ Image Machine/Modules/Machine Data/Cells/MachineItemTableCell.swift
@@ -11,14 +11,31 @@ class MachineItemTableCell: UITableViewCell {
 
     @IBOutlet weak var machineNameLabel: UILabel!
     @IBOutlet weak var machineTypeLabel: UILabel!
+    @IBOutlet weak var testbutton: UIButton!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         setupView()
+        setupMenu()
     }
     
     private func setupView() {
         selectionStyle = .none
+    }
+    
+    func setupMenu() {
+        
+        let edit = UIAction(title: "Edit", image: UIImage(systemName: "pencil")) { _ in
+            print("Edit")
+        }
+        
+        let delete = UIAction(title: "Delete", image: UIImage(systemName: "minus")) { _ in
+            print("Delete")
+        }
+        
+        let menu = UIMenu(title: "Menu", children: [edit, delete])
+        testbutton.menu = menu
+        testbutton.showsMenuAsPrimaryAction = true
     }
     
     func bind(name: String, type: String) {
