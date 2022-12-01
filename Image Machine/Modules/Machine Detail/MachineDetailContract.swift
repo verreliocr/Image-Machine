7c48369d8ac444bc0824ac20ce5649cb5abe54b1
verrelio.cr <verrelioragna@gmail.com>
2022-12-01 23:54:41 +0700

    [Verrel] add contract for detail machine module

---
+++ Image Machine/Modules/Machine Detail/MachineDetailContract.swift
@@ -0,0 +1,24 @@
+//
+//  MachineDetailContract.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import Foundation
+import UIKit
+
+protocol IMachineDetailView: AnyObject {
+    func showMessage(_ message: String, title: String, completion: (() -> Void)?)
+    func reloadData()
+}
+
+protocol IMachineDetailViewModel {
+    func setView(_ view: IMachineDetailView)
+    func viewWillAppearing()
+    func getId() -> String
+    func getName() -> String
+    func getType() -> String
+    func getQRCodeNumber() -> Int
+    func getLastMaintenance() -> String
+}
