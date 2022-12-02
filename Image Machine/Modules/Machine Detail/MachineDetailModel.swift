4a1ec3f3ff42191b3999d36e939739880e87b453
verrelio.cr <verrelioragna@gmail.com>
2022-12-02 21:12:31 +0700

    [Verrel] create model for machine detail module

---
+++ Image Machine/Modules/Machine Detail/MachineDetailModel.swift
@@ -0,0 +1,16 @@
+//
+//  MachineDetailModel.swift
+//  Image Machine
+//
+//  Created by Verrelio C. Rizky on 01/12/22.
+//
+
+import Foundation
+
+class MachineDetailModel {
+    var machineData: MachineObject
+    
+    init(machineData: MachineObject) {
+        self.machineData = machineData
+    }
+}
